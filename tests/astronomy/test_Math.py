d80fbcfd35f60a131aadd66d025b94c64be09108
alphahm <ahmwebdev@gmail.com>
2022-08-06 23:33:58 +0100

    some refactoring of math tests

--- tests/astronomy/test_Math.py
+++ tests/astronomy/test_Math.py
@@ -5,72 +5,80 @@ from adhanpy.util.TimeComponents import TimeComponents
 import adhanpy.util.CalendarUtil as CalendarUtil
 
 
-def test_normalizing():
-    assert FloatUtil.normalize_with_bound(2.0, -5) == pytest.approx(-3, abs=1e-5)
-    assert FloatUtil.normalize_with_bound(-4.0, -5.0) == pytest.approx(-4, abs=1e-5)
-    assert FloatUtil.normalize_with_bound(-6.0, -5.0) == pytest.approx(-1, abs=1e-5)
-
-    assert FloatUtil.normalize_with_bound(-1.0, 24) == pytest.approx(23, abs=1e-5)
-    assert FloatUtil.normalize_with_bound(1.0, 24.0) == pytest.approx(1, abs=1e-5)
-    assert FloatUtil.normalize_with_bound(49.0, 24) == pytest.approx(1, abs=1e-5)
-
-    assert FloatUtil.normalize_with_bound(361.0, 360) == pytest.approx(1, abs=1e-5)
-    assert FloatUtil.normalize_with_bound(360.0, 360) == pytest.approx(0, abs=1e-5)
-    assert FloatUtil.normalize_with_bound(259.0, 360) == pytest.approx(259, abs=1e-5)
-    assert FloatUtil.normalize_with_bound(2592.0, 360) == pytest.approx(72, abs=1e-5)
-
-    assert FloatUtil.unwind_angle(-45.0) == pytest.approx(315, abs=1e-5)
-    assert FloatUtil.unwind_angle(361.0) == pytest.approx(1, abs=1e-5)
-    assert FloatUtil.unwind_angle(360.0) == pytest.approx(0, abs=1e-5)
-    assert FloatUtil.unwind_angle(259.0) == pytest.approx(259, abs=1e-5)
-    assert FloatUtil.unwind_angle(2592.0) == pytest.approx(72, abs=1e-5)
-
-    assert FloatUtil.normalize_with_bound(360.1, 360) == pytest.approx(0.1, abs=1e-2)
-
-
-def test_closest_angle():
-    assert FloatUtil.closest_angle(360.0) == pytest.approx(0, abs=1e-6)
-    assert FloatUtil.closest_angle(361.0) == pytest.approx(1, abs=1e-6)
-    assert FloatUtil.closest_angle(1.0) == pytest.approx(1, abs=1e-6)
-    assert FloatUtil.closest_angle(-1.0) == pytest.approx(-1, abs=1e-6)
-    assert FloatUtil.closest_angle(-181.0) == pytest.approx(179, abs=1e-6)
-    assert FloatUtil.closest_angle(180.0) == pytest.approx(180, abs=1e-6)
-    assert FloatUtil.closest_angle(359.0) == pytest.approx(-1, abs=1e-6)
-    assert FloatUtil.closest_angle(-359.0) == pytest.approx(1, abs=1e-6)
-    assert FloatUtil.closest_angle(1261.0) == pytest.approx(-179, abs=1e-6)
-    assert FloatUtil.closest_angle(-360.1) == pytest.approx(-0.1, abs=1e-2)
+@pytest.mark.parametrize(
+    "value, max, expected, tolerance",
+    [
+        (2.0, -5, -3, 1e-5),
+        (-4.0, -5.0, -4, 1e-5),
+        (-6.0, -5.0, -1, 1e-5),
+        (-1.0, 24, 23, 1e-5),
+        (1.0, 24.0, 1, 1e-5),
+        (49.0, 24, 1, 1e-5),
+        (361.0, 360, 1, 1e-5),
+        (360.0, 360, 0, 1e-5),
+        (259.0, 360, 259, 1e-5),
+        (2592.0, 360, 72, 1e-5),
+        (360.1, 360, 0.1, 1e-2),
+    ],
+)
+def test_normalize_with_bound(value, max, expected, tolerance):
+    assert FloatUtil.normalize_with_bound(value, max) == pytest.approx(
+        expected, abs=tolerance
+    )
 
 
-def test_TimeComponents():
-    comps1 = TimeComponents.from_float(15.199)
-    assert comps1 is not None
-    assert comps1.hours == 15
-    assert comps1.minutes == 11
-    assert comps1.seconds == 56
-
-    comps2 = TimeComponents.from_float(1.0084)
-    assert comps2 is not None
-    assert comps2.hours == 1
-    assert comps2.minutes == 0
-    assert comps2.seconds == 30
-
-    comps3 = TimeComponents.from_float(1.0083)
-    assert comps3 is not None
-    assert comps3.hours == 1
-    assert comps3.minutes == 0
-
-    comps4 = TimeComponents.from_float(2.1)
-    assert comps4 is not None
-    assert comps4.hours == 2
-    assert comps4.minutes == 6
-
-    comps5 = TimeComponents.from_float(3.5)
-    assert comps5 is not None
-    assert comps5.hours == 3
-    assert comps5.minutes == 30
+@pytest.mark.parametrize(
+    "value, expected",
+    [
+        (-45.0, 315),
+        (361.0, 1),
+        (360.0, 0),
+        (259.0, 259),
+        (2592.0, 72),
+    ],
+)
+def test_unwind_angle(value, expected):
+    assert FloatUtil.unwind_angle(value) == pytest.approx(expected, abs=1e-5)
 
 
-def testMinuteRounding():
+@pytest.mark.parametrize(
+    "angle, expected, tolerance",
+    [
+        (360.0, 0, 1e-6),
+        (361.0, 1, 1e-6),
+        (1.0, 1, 1e-6),
+        (-1.0, -1, 1e-6),
+        (-181.0, 179, 1e-6),
+        (180.0, 180, 1e-6),
+        (359.0, -1, 1e-6),
+        (-359.0, 1, 1e-6),
+        (1261.0, -179, 1e-6),
+        (-360.1, -0.1, 1e-2),
+    ],
+)
+def test_closest_angle(angle, expected, tolerance):
+    assert FloatUtil.closest_angle(angle) == pytest.approx(expected, abs=tolerance)
+
+
+@pytest.mark.parametrize(
+    "value, hours, minutes, seconds",
+    [
+        (15.199, 15, 11, 56),
+        (1.0084, 1, 0, 30),
+        (1.0083, 1, 0, 29),
+        (2.1, 2, 6, 0),
+        (3.5, 3, 30, 0),
+    ],
+)
+def test_TimeComponents(value, hours, minutes, seconds):
+    components = TimeComponents.from_float(value)
+    assert components is not None
+    assert components.hours == hours
+    assert components.minutes == minutes
+    assert components.seconds == seconds
+
+
+def test_minute_rounding():
     comps1 = datetime(2015, 1, 1, 10, 2, 29, tzinfo=timezone.utc)
     rounded1 = CalendarUtil.rounded_minute(comps1)
 
