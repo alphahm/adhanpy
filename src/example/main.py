cf13e766a0564438a5f1291748737d97091aa0a2
alphahm <ahmwebdev@gmail.com>
2022-08-06 14:55:47 +0100

    update the example using the time_zone arg

--- src/example/main.py
+++ src/example/main.py
@@ -4,23 +4,27 @@ from adhanpy.calculation import CalculationMethod
 from adhanpy.PrayerTimes import PrayerTimes
 
 
-def print_prayer_times(prayer_times: PrayerTimes, time_zone: ZoneInfo):
-    print(f"Prayer times for {today.astimezone(london_zone).strftime('%A %d %B %Y')}:")
-    print(f"Fajr: {prayer_times.fajr.astimezone(london_zone).strftime('%H:%M')}")
-    print(f"Sunrise: {prayer_times.sunrise.astimezone(london_zone).strftime('%H:%M')}")
-    print(f"Dhuhr: {prayer_times.dhuhr.astimezone(london_zone).strftime('%H:%M')}")
-    print(f"Asr: {prayer_times.asr.astimezone(london_zone).strftime('%H:%M')}")
-    print(f"Maghrib: {prayer_times.maghrib.astimezone(london_zone).strftime('%H:%M')}")
-    print(f"Isha: {prayer_times.isha.astimezone(london_zone).strftime('%H:%M')}")
+def print_prayer_times(when: datetime, prayer_times: PrayerTimes):
+    format = "%H:%M"
+    print(f"Prayer times for {today.strftime('%A %d %B %Y')}:")
+    print(f"Fajr: {prayer_times.fajr.strftime(format)}")
+    print(f"Sunrise: {prayer_times.sunrise.strftime(format)}")
+    print(f"Dhuhr: {prayer_times.dhuhr.strftime(format)}")
+    print(f"Asr: {prayer_times.asr.strftime(format)}")
+    print(f"Maghrib: {prayer_times.maghrib.strftime(format)}")
+    print(f"Isha: {prayer_times.isha.strftime(format)}")
 
 
 if __name__ == "__main__":
     coordinates = (51.49799827422162, -0.1358135027951458)
 
     today = datetime.now()
-    prayer_times = PrayerTimes(
-        coordinates, today, CalculationMethod.MOON_SIGHTING_COMMITTEE
-    )
     london_zone = ZoneInfo("Europe/London")
+    prayer_times = PrayerTimes(
+        coordinates,
+        today,
+        CalculationMethod.MOON_SIGHTING_COMMITTEE,
+        time_zone=london_zone,
+    )
 
-    print_prayer_times(prayer_times, london_zone)
+    print_prayer_times(today, prayer_times)
